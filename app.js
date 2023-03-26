5d3df555d89b3325593cdeadc53650148fce011d
j-rivs <jrivs15@terpmail.umd.edu>
2023-03-26 13:34:33 -0400

    Add general functionality of the app

---
+++ app.js
@@ -0,0 +1,55 @@
+// Selectors
+const todoInput = document.querySelector('.input');
+const submitButton = document.querySelector('.submit-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+submitButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+// Functions
+function addTodo(event) {
+    // Prevent form submission
+    event.preventDefault();
+    // Todo Div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Check button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa-sharp fa-solid fa-square-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // Delete button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fa-sharp fa-solid fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    // Append to list
+    todoList.appendChild(todoDiv);
+    // Clear todo input value
+    todoInput.value = "";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    // Delete
+    if(item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        // Animation
+        todo.classList.add('drop');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+
+    // Checkmark
+    if(item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
