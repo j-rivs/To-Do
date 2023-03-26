ddf449ea90c7c2fbd0bc7b6c25270adccbe1a8e8
j-rivs <jrivs15@terpmail.umd.edu>
2023-03-26 14:33:29 -0400

    Finalize filter option

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function addTodo(event) {
     event.preventDefault();
     // Todo Div
     const todoDiv = document.createElement('div');
-    todoDiv.classList.add('todo');
+    todoDiv.classList.add('todos');
     // Create li
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
@@ -41,7 +41,7 @@ function deleteCheck(e) {
     const item = e.target;
     // Delete
     if(item.classList[0] === "trash-btn") {
-        const todo = item.parentElement;
+        const todos = item.parentElement;
         // Animation
         todo.classList.add('drop');
         todo.addEventListener('transitionend', function(){
@@ -56,6 +56,27 @@ function deleteCheck(e) {
     }
 }
 
-function filterTodo(event) {
+function filterTodo(e) {
     const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
 }
